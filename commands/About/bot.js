8d17e1f155d0448ded5c86dbf9083c20189fcaf2
Brandon Wright <71175207+branwright1@users.noreply.github.com>
2020-10-05 11:19:14 +0100

    Update bot.js

--- commands/About/bot.js
+++ commands/About/bot.js
@@ -2,15 +2,15 @@ const Discord = require("discord.js");
 
 module.exports = {
   name: "about",
-    description: "Get some info about Opal",
+    description: "Get some info about Brainwash",
     category: "info",
     usage: "about",
   run: async(client, message, args) => {
 
     const bot = new Discord.MessageEmbed()
     .setTitle(`About me`)
-    .setColor("#AAC4C4")
-    .setDescription("Hi! My name is Opal, I'm infobot with knowledge about Retro Consoles, Personal Computers and Operating Systems.")
+    .setColor("#f3ce4e")
+    .setDescription("Hi! My name is Brainwash, I'm infobot with knowledge about Retro Consoles, Personal Computers and Operating Systems.")
     .setTimestamp()
     .setFooter(message.guild.name, message.guild.iconURL())
     message.channel.send(bot);
